5591342bdf6c3f44d988f9077a0cedcc0b3d7698
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-07-08 23:40:17 -0500

    remove unused plugins and remaps

--- init.lua
+++ init.lua
@@ -906,7 +906,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
@@ -942,5 +942,3 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --
--- nvim tree remaps
-require 'custom.plugins.nvim-tree'
--- lua/custom/plugins/nvim-tree.lua
+++
@@ -1,4 +0,0 @@
-local nvimtree = require 'nvim-tree.api'
-
-vim.keymap.set('n', '<C-f>', nvimtree.tree.open)
--- vim.keymap.set('v', '<C-f>', nvimtree.tree.open)
