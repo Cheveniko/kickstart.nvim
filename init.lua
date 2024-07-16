65eb81cd1b2624597cbf50beae28b878c389116f
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-07-16 00:26:42 -0500

    restore remap for opening nvimtree

--- init.lua
+++ init.lua
@@ -942,3 +942,5 @@ require('lazy').setup({
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 --
+-- nvim tree remaps
+require 'custom.plugins.nvim-tree'
---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,3 @@
+local nvimtree = require 'nvim-tree.api'
+
+vim.keymap.set('n', '<C-f>', nvimtree.tree.open)
